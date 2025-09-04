96f46444e5c119263bdddbf4a4e83232597290b2
langaludidi <langaludidi@gmail.com>
2025-09-04 16:36:33 +0200

    Optimize chunk size in vite.config.js with manualChunks and increase chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/firestore', 'firebase/auth'],
+          chartjs: ['chart.js', 'react-chartjs-2']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Increase limit to 1000 kB to suppress warning
   }
 });
